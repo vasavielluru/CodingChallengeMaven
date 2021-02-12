353aa7696fd427e9ba861bfe0bac3b06931aff65
vasavi elluru <elluruvasavi.19993@gmail.com>
2021-02-12 16:52:49 +0530

    Maven Project Comitting to github

---
+++ NewMavenClass/src/main/java/Maven_Practice/NewMavenClass/App.java
@@ -0,0 +1,13 @@
+package Maven_Practice.NewMavenClass;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
